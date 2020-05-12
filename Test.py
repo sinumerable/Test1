512f247a9e38bf45839eca20275d0fb56ebfe35e
sinumerable <65259079+sinumerable@users.noreply.github.com>
2020-05-12 18:47:53 -0400

    Initial commit

---
+++ Test.py
@@ -0,0 +1,11 @@
+print("hello world")
+print(1+2)
+print('hello world')
+print("""hello
+world
+fuck""")
+print("fuck you too")
+a = 'hello "world"'
+a = 'this is a quote : \ " "
+filePath = "C:\\windows\\system32"
+print(‘Hello “world” ‘)
